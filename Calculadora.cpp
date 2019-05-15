9eca85f1e19709cf4871e1d664d4af09f0f174b3
Paula Ribeiro <paula.mribeiro05@gmail.com>
2019-05-15 15:20:57 -0300

    Commit inicial

---
+++ Calculadora.cpp
@@ -0,0 +1,23 @@
+#import "Calculadora.h"
+#import "math.h"
+
+int soma(int operadorA, int operadorB) {
+    return operadorA + operadorB;
+}
+
+int subtracao(int minuendo, int subtraendo) {
+    return minuendo - subtraendo;
+}
+
+int divisao(int numerador, int denominador) {
+    return numerador/denominador;
+}
+
+int multiplicacao(int operadorA, int operadorB) {
+    return operadorA*operadorB;
+}
+
+int potenciacao(int base, int potencia) {
+    return 0;
+}
+
