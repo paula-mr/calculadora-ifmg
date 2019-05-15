a38d8e42d5aaa2f5b5e48165e893659e92555cfb
Paula Ribeiro <paula.mribeiro05@gmail.com>
2019-05-15 15:29:26 -0300

    Corrige função de divisão

--- Calculadora.cpp
+++ Calculadora.cpp
@@ -8,8 +8,11 @@ int subtracao(int minuendo, int subtraendo) {
     return minuendo - subtraendo;
 }
 
-int divisao(int numerador, int denominador) {
-    return numerador/denominador;
+float divisao(int numerador, int denominador) {
+    if (denominador == 0)
+        return 0;
+
+    return (float)numerador/(float)denominador;
 }
 
 int multiplicacao(int operadorA, int operadorB) {
